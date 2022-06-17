91b5d8da822dd5fcb7947539fcac379646c53abe
btw31 <wallenhorst.6@buckeyemail.osu.edu>
2022-06-17 10:21:34 -0400

    Add shutdown attempt to cli

--- cli.py
+++ cli.py
@@ -1,9 +1,14 @@
 import socket
+import os
 
 # Connect to battery server and request charge. Printing it at end
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientsocket.connect(('192.168.1.78', 8089))
 clientsocket.send('get'.encode())
 buf = clientsocket.recv(64)
+charge = 0
 if len(buf) > 0:
-    print(buf)
+    charge = float(buf.decode().strip(" Percent"))
+
+if charge >= 100:
+    os.system("shutdown now")
